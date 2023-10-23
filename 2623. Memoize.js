fb0791a5b7c0c35542ef1d72abc1b48e03062061
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2023-10-23 15:00:32 +0600

    done

---
+++ 2623. Memoize.js
@@ -0,0 +1,35 @@
+let count = 0;
+const memoize = (fn) => {
+    const cashes = {};
+
+    return (...args) => {
+        const argKeys = JSON.stringify(...args);
+        console.log('argKey: ', argKeys);
+        console.log('arguments ', ...args)
+        console.log('cashes', cashes)
+
+        if (argKeys in cashes) {
+            return cashes[argKeys];
+        } else {
+            const result = fn(...args);
+            cashes[argKeys] = result;
+            count++;
+            return `result is ${result}`;
+        }
+    };
+};
+
+const getCallCount = () => {
+    return count;
+};
+const sum = (a, b) => a + b;
+const memoizedSum = memoize(sum);
+
+console.log(memoizedSum(2, 2));
+console.log(memoizedSum(2, 2));
+console.log('total: ', getCallCount());
+
+const factorial = (n) => (n <= 1 ? 1 : n * factorial(n - 1));
+const memoFactorial = memoize(factorial);
+console.log(memoFactorial(2));
+console.log(memoFactorial(3));
